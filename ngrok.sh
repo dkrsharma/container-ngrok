56f6b9389d7ae52d1198492067ae4b988d7cbbcd
Dhirendra Kumar Sharma <dkrsharma3@gmail.com>
2021-01-14 13:35:14 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,4 +1,5 @@
-apt update && apt install -y openssh-server unzip &&
+apt update && 
+apt install -y openssh-server unzip &&
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip &&
 unzip ngrok-stable-linux-amd64.zip -d /bin &&
 ngrok authtoken 1i2svlTAFSEATIDgqAVm3XjmOlI_2SQecTizgGLqQPUHykBy7 &&
