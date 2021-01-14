e4db39c6ec3712fa16a96a8325a095e0b4a80db0
Dhirendra Kumar Sharma <dkrsharma3@gmail.com>
2021-01-14 13:13:51 +0530

    Add files via upload

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,7 @@
-npm install ngrok
-const ngrok = require('ngrok');
-(async function() {
-  const url = await ngrok.connect({proto: 'tcp', addr: 22}); // tcp://0.tcp.ngrok.io:48590
-})();
+apt update && apt install -y openssh-server unzip 
+
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip -d /bin
+ngrok authtoken 1i2svlTAFSEATIDgqAVm3XjmOlI_2SQecTizgGLqQPUHykBy7
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+service ssh restart
