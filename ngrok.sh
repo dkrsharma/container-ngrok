b0cb99e2e40517464e94bae91734dd5ebb36ef35
Dhirendra Kumar Sharma <dkrsharma3@gmail.com>
2021-01-14 13:19:43 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,8 +1,7 @@
-apt update && apt install -y openssh-server unzip 
-
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip -d /bin
-ngrok authtoken 1i2svlTAFSEATIDgqAVm3XjmOlI_2SQecTizgGLqQPUHykBy7
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-service ssh restart
+apt update && apt install -y openssh-server unzip &&
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip &&
+unzip ngrok-stable-linux-amd64.zip -d /bin &&
+ngrok authtoken 1i2svlTAFSEATIDgqAVm3XjmOlI_2SQecTizgGLqQPUHykBy7 &&
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config &&
+service ssh restart &&
 ngrok tcp 22
